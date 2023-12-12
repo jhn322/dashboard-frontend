48ea0513e8d0dc8f105058c98b5eaaf5f43ef363
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-12 23:01:02 +0100

    declared inputField outside function, added evenlistener & function for enter key to update h1

--- js/dashboardHeadline.js
+++ js/dashboardHeadline.js
@@ -3,34 +3,51 @@
 // Get the headline by id
 const userHeadline = document.getElementById("userSpecificHeadline");
 
+// Declare inputField outside the function
+let inputField;
+
 // Arrow function to handle click event
 const changeHeadline = () => {
   // Get the current text content of the h1
   const currentText = userHeadline.textContent;
 
   // Creates an input field for the user
-  const inputField = document.createElement("input");
+  inputField = document.createElement("input");
   inputField.type = "text";
 
   // Clears the h1 and replaces it with input field
   userHeadline.textContent = "";
   userHeadline.appendChild(inputField);
+  inputField.value = currentText === "[Click to Edit]" ? "" : currentText;
 
   // Selects the input field for instant type in
   inputField.select();
 
   // For when the input field loses focus
-  inputField.addEventListener("blur", () => {
-    // Get the text from the input field
-    const newText = inputField.value;
+  inputField.addEventListener("blur", updateHeadline);
 
-    // Sets the final text or default if empty
-    const finalText = newText === "" ? "[Click to Edit]" : newText;
-    userHeadline.textContent = finalText;
-
-    // Saves to localStorage
-    localStorage.setItem("userHeadline", finalText);
+  // Listen to enter key to allow press on input field
+  inputField.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      updateHeadline();
+    }
   });
+};
+
+// Function to update the h1
+const updateHeadline = () => {
+  // Get the text from the input field
+  const newText = inputField.value;
+
+  // Sets the final text or default if empty
+  const finalText = newText === "" ? "[Click to Edit]" : newText;
+  userHeadline.textContent = finalText;
+
+  // Saves to localStorage
+  localStorage.setItem("userHeadline", finalText);
+
+  // Removes input field upon updating h1
+  inputField.remove();
 };
 
 // Arrow function to load the saved text from localStorage
