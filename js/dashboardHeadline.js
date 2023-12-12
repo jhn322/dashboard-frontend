210181a483d0a48f88e41c70188eeeb3ac52af3e
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-12 22:41:39 +0100

    select method, clear h1 for input, reset to default if no input is written, save to localstorage

--- js/dashboardHeadline.js
+++ js/dashboardHeadline.js
@@ -11,4 +11,24 @@ const changeHeadline = () => {
   // Creates an input field for the user
   const inputField = document.createElement("input");
   inputField.type = "text";
+
+  // Clears the h1 and replaces it with input field
+  userHeadline.textContent = "";
+  userHeadline.appendChild(inputField);
+
+  // Selects the input field for instant type in
+  inputField.select();
+
+  // For when the input field loses focus
+  inputField.addEventListener("blur", () => {
+    // Get the text from the input field
+    const newText = inputField.value;
+
+    // Sets the final text or default if empty
+    const finalText = newText === "" ? "[Click to Edit]" : newText;
+    userHeadline.textContent = finalText;
+
+    // Saves to localStorage
+    localStorage.setItem("userHeadline", finalText);
+  });
 };
