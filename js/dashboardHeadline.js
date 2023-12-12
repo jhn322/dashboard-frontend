318fb5010bb3de4771c11191f2390ac16976b6ed
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-12 22:09:29 +0100

    created new js to input headline

---
+++ js/dashboardHeadline.js
@@ -0,0 +1,4 @@
+/* The purpose of this function is to allow the user to change the headline to whatever they want, be it their name or something else. This allows for a bit more personalized dashboard landing page. */
+
+// Get the h1 element
+const headline = document.getElementById("userSpecificHeadline");
