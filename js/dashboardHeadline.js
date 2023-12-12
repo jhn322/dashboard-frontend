0337cc6f1dedb854c6c3bf07ca233d6aee9b4aed
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-12 22:24:40 +0100

    created function, get the text content and create input field

--- js/dashboardHeadline.js
+++ js/dashboardHeadline.js
@@ -1,4 +1,14 @@
-/* The purpose of this function is to allow the user to change the headline to whatever they want, be it their name or something else. This allows for a bit more personalized dashboard landing page. */
+/* The purpose of this function is to allow the user to change the headline to whatever they want by simply clicking on it. Be it their name or something else. This allows for a bit more personalized dashboard landing page. */
 
-// Get the h1 element
-const headline = document.getElementById("userSpecificHeadline");
+// Get the headline by id
+const userHeadline = document.getElementById("userSpecificHeadline");
+
+// Arrow function to handle click event
+const changeHeadline = () => {
+  // Get the current text content of the h1
+  const currentText = userHeadline.textContent;
+
+  // Creates an input field for the user
+  const inputField = document.createElement("input");
+  inputField.type = "text";
+};
