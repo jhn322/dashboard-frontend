e109b909140ca93450efc7b82a00dada4d49d031
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-12 21:13:12 +0100

    variables to get time & date as a string using get() method and padStart() method to always show 2 numbers

--- js/timedate.js
+++ js/timedate.js
@@ -8,4 +8,14 @@ const updateClock = () => {
   // Get the H3 and P tag from the HTML by their ID
   const timeH3 = document.getElementById("time-h3");
   const dateP = document.getElementById("date-p");
+
+  // Get the hours, minutes and seconds in current time
+  const hours = String(currentTime.getHours()).padStart(2, "0");
+  const minutes = String(currentTime.getMinutes()).padStart(2, "0");
+  const seconds = String(currentTime.getSeconds()).padStart(2, "0");
+
+  // Get the current day, month and year.
+  const day = String(currentTime.getDate()).padStart(2, "0");
+  const month = String(currentTime.getMonth() + 1).padStart(2, "0");
+  const year = currentTime.getFullYear();
 };
