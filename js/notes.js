355e1b5c47daa60ab2559d17ff55c06f16cef971
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-13 02:42:20 +0100

    added confirm prompt if delete is pressed, removes from localstorage and comments

--- js/notes.js
+++ js/notes.js
@@ -1,20 +1,32 @@
 // With notes the user has the ability to quickly write anything on their mind down and not be afraid it disappears if the web page is closed down. Along with saving, there's also a delete button if you wanna start over.
 
+// Waits for the DOM element to load
 document.addEventListener("DOMContentLoaded", () => {
+  // Gets the text area by id
   const notesTextarea = document.getElementById("notes-dash");
+  // Gets the delete button by id
   const deleteButton = document.getElementById("notes-delete-btn");
 
+  // Saves the note in localstorage and retrieves them on reload
   const savedNotes = localStorage.getItem("userNotes");
   if (savedNotes) {
     notesTextarea.value = savedNotes;
   }
 
+  // Saves the input to localstorage upon typing anything
   notesTextarea.addEventListener("input", () => {
     localStorage.setItem("userNotes", this.value);
   });
 
+  // Arrow function to listen for click on the delete button
   deleteButton.addEventListener("click", () => {
-    localStorage.removeItem("userNotes");
-    notesTextarea.value = "";
+    // Displays a confirmation prompt upon pressing delete
+    const confirmation = confirm("Are you sure you want to delete your notes?");
+    // If user press "OK" notes will be removed from localstorage
+    if (confirmation) {
+      localStorage.removeItem("userNotes");
+      // Clears the textarea
+      notesTextarea.value = "";
+    }
   });
 });
