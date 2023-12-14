576a3de5b40c2ae846e3b871154e7d33293acba9
Johan Söderlund <johan.soderlund96@gmail.com>
2023-12-14 03:47:29 +0100

    more styling weather and div for p to style seperately

--- js/weather.js
+++ js/weather.js
@@ -105,10 +105,12 @@ const displayWeatherData = (
 
   // Show weather info on the dashboard page
   weatherInfo.innerHTML = `
-    <h3>${title}</h3>
-    <p>${temperatureCelsius.toFixed(2)}°C</p>
+  <h3>${title}</h3>
+  <div class="weather-details">
+    <p>${temperatureCelsius.toFixed(2)}°C | </p>
     <p>${description}</p>
-  `;
+  </div>
+`;
 };
 
 // Calls the weather function
